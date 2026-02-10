580b0e2c32cf6babaeaea9632402f9d0a9828fcb
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-02-10 11:35:48 -0300

    Base setada

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,20 @@ function App() {
       <Header />
       <main>
         <section>
-          
+          <article className='preview'>
+            <div className='item-preview'>
+              <h2>Entradas</h2>
+              <p>R$ 0.000,00</p>
+            </div>
+            <div className='item-preview'>
+              <h2>Saídas</h2>
+              <p>R$ 0.000,00</p>
+            </div>
+            <div className='item-preview'>
+              <h2>Situação</h2>
+              <p>R$ 0.000,00</p>
+            </div>
+          </article>
         </section>
       </main>
       <Footer />
