8bb3894ce7faaf8416dc54d8943691f98effb4aa
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-03-29 09:30:13 -0300

    Responsividade da tabela aprimorada

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,69 +1,73 @@
-import React, {useState} from "react"
+import {useState} from "react"
 
 function Inputs(props) {
-    const [dados, setDados] = useState({
-        descricao: "",
-        valor: "",
-        tipo: ""
-    })
-    function capturarMudanca(e) {
-        let nameInput = e.target.name
-        let valueInput = e.target.value
-        setDados((prevValue)=>{
-            return {
-                ...prevValue,
-                [nameInput]: valueInput
-            }
-        })
-    }
-    function enviarDados(e) {
-        e.preventDefault()
-        props.onAdd(dados)
-        setDados({
-            descricao: "",
-            valor: "",
-            tipo: ""
-        })
-    }
-    return (    
-        <article className="inputs">
-            <form onSubmit={enviarDados}>
-                <div className="campo_input">
-                    <label className="inputs-maiores">Descrição </label>
-                    <input 
-                        name="descricao" 
-                        value={dados.descricao} 
-                        type="text" required
-                        onChange={capturarMudanca} />
-                </div>
-                <div className="campo_input">
-                    <label className="inputs-maiores">Valor </label>
-                    <input 
-                        name="valor" 
-                        value={dados.valor} 
-                        type="number" required
-                        onChange={capturarMudanca} />
-                </div>
-                <div className="campo_input">
-                    <label> Entrada </label>
-                    <input 
-                        name="tipo" 
-                        value="entrada" 
-                        checked={dados.tipo==="entrada"}
-                        type="radio" required 
-                        onChange={capturarMudanca} />
-                    <label> Saída </label>
-                    <input 
-                        name="tipo" 
-                        value="saida" 
-                        type="radio" required 
-                        checked={dados.tipo==="saida"}
-                        onChange={capturarMudanca} />
-                </div>
-                <button type="submit">Salvar</button>
-            </form>
-        </article>
-    )
+	const [dados, setDados] = useState({
+		descricao: "",
+		valor: "",
+		tipo: ""
+	})
+	function capturarMudanca(e) {
+		let nameInput = e.target.name
+		let valueInput = e.target.value
+		setDados((prevValue)=>{
+			return {
+				...prevValue,
+				[nameInput]: valueInput
+			}
+		})
+	}
+	function enviarDados(e) {
+		e.preventDefault()
+		props.onAdd(dados)
+		setDados({
+			descricao: "",
+			valor: "",
+			tipo: ""
+		})
+	}
+	return (    
+		<article className="inputs">
+			<form onSubmit={enviarDados}>
+				<div className="campo_input">
+					<label className="inputs-maiores">Descrição </label>
+					<input 
+						name="descricao" 
+						value={dados.descricao} 
+						type="text" required
+						onChange={capturarMudanca} 
+					/>
+				</div>
+				<div className="campo_input">
+					<label className="inputs-maiores">Valor </label>
+					<input 
+						name="valor" 
+						value={dados.valor} 
+						type="number" required
+						onChange={capturarMudanca} 
+					/>
+				</div>
+				<div className="campo_input">
+					<label> Entrada </label>
+					<input 
+						name="tipo" 
+						value="entrada" 
+						checked={dados.tipo==="entrada"}
+						type="radio" required 
+						onChange={capturarMudanca} 
+					/>
+					<label> Saída </label>
+					<input 
+						name="tipo" 
+						value="saida" 
+						type="radio" required 
+						checked={dados.tipo==="saida"}
+						onChange={capturarMudanca} 
+					/>
+				</div>
+					<button type="submit">Salvar</button>
+			</form>
+		</article>
+	)
 }
 
 export default Inputs
--- src/components/Tabela.js
+++ src/components/Tabela.js
@@ -1,28 +1,33 @@
-import React from "react"
 
 function Tabela(props) {
-    return (
-        <article className="tabela">
-            <table>
-                <tr>
-                    <th>Descrição</th>
-                    <th>Valor</th>
-                    <th>Tipo</th>
-                    <th>Excluir</th>
-                </tr>
-                {props.lista.map((_,i)=>{
-                    return (
-                        <tr>
-                            <td>{_.descricao}</td>
-                            <td>{_.valor}</td>
-                            <td>{_.tipo}</td>
-                            <td onClick={()=>{ props.onDelete(i) }}><i class="fa-solid fa-trash"></i></td>
-                        </tr>
-                    )
-                })}
-            </table>
-        </article>
-    )
+	return (
+		<article className="tabela">
+			<table>
+				<thead>
+					<tr>
+							<th>Descrição</th>
+							<th>Valor</th>
+							<th>Tipo</th>
+							<th>Excluir</th>
+					</tr>
+				</thead>
+				<tbody>
+					{props.lista.map((_,i)=>{
+						return (
+							<tr>
+								<td data-label="Descrição:">{_.descricao}</td>
+								<td data-label="Valor:">{_.valor}</td>
+								<td data-label="Tipo:">{_.tipo}</td>
+								<td data-label="Excluir:" onClick={()=>{ props.onDelete(i) }}>
+									<i class="fa-solid fa-trash"></i>
+								</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</article>
+	)
 }
 
 export default Tabela
