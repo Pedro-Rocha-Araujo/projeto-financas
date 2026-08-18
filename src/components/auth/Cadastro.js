88836ff69298f0c3179d83eb319baec162fe6789
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-08-18 19:00:01 -0300

    Função de cadastrar usuário implementada

--- src/components/auth/Cadastro.js
+++ src/components/auth/Cadastro.js
@@ -1,7 +1,23 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import "./auth.css"
 
+import { auth } from "../../firebaseConnection"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+
 export default function Cadastro() {
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
+  async function cadastrarUsuario(e) {
+    e.preventDefault()
+    try {
+      await createUserWithEmailAndPassword(auth, email, senha)
+    } catch(erro) {
+      console.log(erro)
+    }
+  }
+
   return (
     <div className="background">
 
@@ -11,11 +27,17 @@ export default function Cadastro() {
           <h1>Cadastro</h1>
         </div>
 
-        <form className="main-auth">
+        <form onSubmit={cadastrarUsuario} className="main-auth">
           <input 
+            type="text"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             placeholder="Digite seu E-mail"
           />
           <input 
+            type="text"
+            value={senha}
+            onChange={(e)=>setSenha(e.target.value)}
             placeholder="Digite sua Senha"
           />
           <button>Cadastrar</button>
